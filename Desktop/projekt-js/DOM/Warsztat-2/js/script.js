eaff40d5b85adaf5f2480aa89391aed9522995ab
mariuszdabrowski79 <mariuszdabrowski79@op.pl>
2023-06-07 12:27:41 +0200

    Warsztat 2 z DOM

---
+++ Desktop/projekt-js/DOM/Warsztat-2/js/script.js
@@ -0,0 +1,10 @@
+const submitForm = function(event) {
+    event.preventDefault();
+
+    let firstName = document.querySelector('[name="fname"]');
+    let lastName = document.querySelector('[name="lname"]');
+    console.log(`Imię: ${firstName.value}, Nazwisko: ${lastName.value}`);
+}
+
+let form = document.getElementById('form'); 
+form.addEventListener('submit', submitForm);
